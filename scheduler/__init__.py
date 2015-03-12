4b8923b71a8e2048b06a44b1e8d9d837232656f6
Thomas <tn90ca@gmail.com>
2015-03-12 16:53:47 -0400

    Create __init__.py

---
+++ scheduler/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# __init__.py
+#
+# Created by Thomas Nelson <tn90ca@gmail.com>
+#
+# Created..........2015-03-12
+# Modified.........2015-03-12
+
+__author__     = 'Thomas Nelson'
+__copyright__  = 'Copyright 2015, Thomas Nelson'
+__credits__    = ['Thomas Nelson']
+__license__    = 'MIT'
+__version__    = '1.0.0'
+__maintainer__ = 'Thomas Nelson'
+__email__      = 'tn90ca@gmail.com'
+__status__     = 'Development'
+
+__all__ = ['Individual', 'Employee', 'Store']
